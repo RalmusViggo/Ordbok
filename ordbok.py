04904fd50a8c4c220e43cd7723aa71b2cd44be6f
“RalmusViggo” <rasmus.lang_holmen@icloud.com>
2025-09-15 13:20:21 +0200

    la til en funksjon som viser alle personene

--- ordbok.py
+++ ordbok.py
@@ -1,6 +1,12 @@
 telefonbok = [
     
 ]
-telefonbok.append({"person1": {"navn": "ola", "nummer": 84083049}})
-telefonbok.append({"person2":{"navn": "per", "nummer": 78298327}})
-print(telefonbok)
+telefonbok.append({"navn": "ola", "nummer": 84083049})
+telefonbok.append({"navn": "per", "nummer": 78298327})
+
+
+def vis_alle():
+    for person in telefonbok:
+        print(person)
+
+vis_alle()
